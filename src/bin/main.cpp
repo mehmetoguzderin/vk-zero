ee744917663f24621291b0a92d8a84d80b833ada
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-04-08 05:16:47 +0300

    Create queue

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -1,33 +1,93 @@
 ﻿#include "lib.h"
 
 #include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
-    auto app_name = "main";
     if (auto result = volkInitialize(); result != VK_SUCCESS) {
         std::cerr << "Failed to initialize Volk. Error: " << result << "\n";
         return 1;
     }
-    vkb::InstanceBuilder builder;
-    auto inst_ret = builder.set_app_name(app_name)
-                        .request_validation_layers()
-                        .use_default_debug_messenger()
-                        .build();
-    if (!inst_ret) {
-        std::cerr << "Failed to create Vulkan instance. Error: "
-                  << inst_ret.error().message() << "\n";
-        return 1;
-    }
-    vkb::Instance vkb_inst = inst_ret.value();
-    volkLoadInstance(vkb_inst.instance);
-    if (auto result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS); result < 0) {
+    if (auto result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS |
+                               SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER);
+        result < 0) {
         std::cerr << "Failed to initialize SDL. Error: " << SDL_GetError()
                   << "\n";
         return 1;
     }
-    SDL_CreateWindow(app_name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                     512, 512, SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
-
+    auto app_name = "main";
+    auto window = SDL_CreateWindow(
+        app_name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 512, 512,
+        SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    uint32_t extensions_count = 0;
+    SDL_Vulkan_GetInstanceExtensions(window, &extensions_count, NULL);
+    std::vector<const char *> extensions{extensions_count};
+    SDL_Vulkan_GetInstanceExtensions(window, &extensions_count,
+                                     extensions.data());
+    vkb::InstanceBuilder instance_builder{};
+    for (auto &extension : extensions) {
+        instance_builder.enable_extension(extension);
+    }
+    vkb::Instance instance;
+    if (auto result = instance_builder.set_app_name(app_name)
+                          .use_default_debug_messenger()
+                          .request_validation_layers()
+                          .build();
+        !result) {
+        std::cerr << "Failed to create Vulkan instance. Error: "
+                  << result.error().message() << "\n";
+        return 1;
+    } else {
+        instance = result.value();
+    }
+    volkLoadInstance(instance.instance);
+    VkSurfaceKHR surface;
+    if (auto result =
+            SDL_Vulkan_CreateSurface(window, instance.instance, &surface);
+        !result) {
+        std::cerr << "Failed to create surface."
+                  << "\n";
+        return 1;
+    }
+    vkb::PhysicalDevice physical_device;
+    if (auto result =
+            vkb::PhysicalDeviceSelector{instance}.set_surface(surface).select();
+        !result) {
+        std::cerr << result.error().message() << "\n";
+        return 1;
+    } else {
+        physical_device = result.value();
+    }
+    vkb::Device device;
+    if (auto result = vkb::DeviceBuilder{physical_device}.build(); !result) {
+        std::cerr << result.error().message() << "\n";
+        return 1;
+    } else {
+        device = result.value();
+    }
+    volkLoadDevice(device.device);
+    vkb::Swapchain swapchain;
+    if (auto result =
+            vkb::SwapchainBuilder{device}
+                .add_format_feature_flags(VK_FORMAT_FEATURE_STORAGE_IMAGE_BIT)
+                .add_image_usage_flags(VK_IMAGE_USAGE_STORAGE_BIT)
+                .build();
+        !result) {
+        std::cerr << result.error().message() << " " << result.vk_result()
+                  << "\n";
+        return 1;
+    } else {
+        swapchain = result.value();
+    }
+    VkQueue queue;
+    if (auto result = device.get_queue(vkb::QueueType::present);
+        !result.has_value()) {
+        std::cerr << "failed to get queue: " << result.error().message()
+                  << "\n";
+        return -1;
+    } else {
+        queue = result.value();
+    }
     SDL_Event event;
     int quit = 0;
     while (!quit) {
