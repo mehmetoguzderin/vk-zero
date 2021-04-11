dd79459e97fad46544db26ebac42c2d313605a0d
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-04-11 10:21:09 +0300

    Implement VMA header

--- src/lib.h
+++ src/lib.h
@@ -9,6 +9,7 @@
 #define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
 #define VMA_VULKAN_VERSION 1001000
+#define VMA_IMPLEMENTATION
 #include "vk_mem_alloc.h"
 
 #include "VkBootstrap.h"
