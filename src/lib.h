3377596adf5dffd9266892b9b260be67f97d0811
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-04-26 16:24:57 +0300

    Extend compatibility types

--- src/lib.h
+++ src/lib.h
@@ -26,6 +26,11 @@
 #include "glm/gtx/compatibility.hpp"
 using namespace glm;
 
+#define uint1 uvec1
+#define uint2 uvec2
+#define uint3 uvec3
+#define uint4 uvec4
+
 #define global
 #define kernel
 
@@ -40,10 +45,20 @@ inline uint32_t get_global_id(uint32_t dimindx) { return GLOBAL_ID[dimindx]; }
 #define int64_t long
 #define uint64_t ulong
 
+#define uvec1 (uint1)
 #define uvec2 (uint2)
-#define ivec2 (int2)
-#define vec4 (float4)
+#define uvec3 (uint3)
 #define uvec4 (uint4)
+
+#define ivec1 (int1)
+#define ivec2 (int2)
+#define ivec3 (int3)
+#define ivec4 (int4)
+
+#define vec1 (float1)
+#define vec2 (float2)
+#define vec3 (float3)
+#define vec4 (float4)
 
 #endif
 
