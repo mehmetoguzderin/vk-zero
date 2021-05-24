a10ceeea000d1443b395c29e2aa633b304bcb017
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-05-24 06:17:40 +0300

    Use auto reference types

--- src/lib.h
+++ src/lib.h
@@ -194,8 +194,8 @@ typedef struct {
     uint64_t inc;
 } pcg32_random_t;
 
-uint32_t pcg32_random_r(pcg32_random_t *rng) {
-    uint64_t oldstate = rng->state;
+uint32_t pcg32_random_r(pcg32_random_t *&rng) {
+    auto oldstate = rng->state;
     rng->state = oldstate * 6364136223846793005ULL + (rng->inc | 1);
     uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
     uint32_t rot = oldstate >> 59u;
