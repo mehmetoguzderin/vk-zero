c8724ee3bad4100e791954f8ebce20884bba8a3e
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-05-24 06:15:07 +0300

    Add initial VkZero abstractions

--- src/lib.h
+++ src/lib.h
@@ -52,33 +52,117 @@ static uint32_t GLOBAL_ID[3]{0, 0, 0};
 
 inline uint32_t get_global_id(uint32_t dimindx) { return GLOBAL_ID[dimindx]; }
 
-struct VkZeroDevice {
-    vk::PhysicalDevice physicalDevice;
-    vk::Device device;
-    vk::PhysicalDeviceProperties properties;
-    vk::PhysicalDeviceFeatures features;
-    std::vector<vk::QueueFamilyProperties> queueFamilyProperties;
+struct VkZeroInstance {
+    vk::Instance instance;
+    std::vector<vk::PhysicalDevice> physicalDevices;
+    std::vector<vk::PhysicalDeviceGroupProperties> physicalDeviceGroups;
 };
 
 struct VkZeroQueue {
-    vk::Queue queue;
     uint32_t familyIndex;
     uint32_t index;
+    float priority;
+    vk::Queue queue;
+    vk::CommandPool commandPool;
 };
 
-VkZeroQueue getGraphicsComputeTransferQueue(const VkZeroDevice &device) {
-    if (device.queueFamilyProperties.size() < 1) {
+struct VkZeroDevice {
+    uint32_t index;
+    vk::Device device;
+    VmaAllocator allocator;
+    vk::DescriptorPool descriptorPool;
+    VkZeroQueue queue;
+};
+
+inline VkZeroInstance
+createInstance(const uint32_t enabledExtensionCount,
+               const char *const *ppEnabledExtensionNames) {
+    vk::ApplicationInfo applicationInfo{
+        .apiVersion = VK_API_VERSION_1_1,
+    };
+    auto instance = vk::createInstance(vk::InstanceCreateInfo{
+        .pApplicationInfo = &applicationInfo,
+        .enabledExtensionCount = enabledExtensionCount,
+        .ppEnabledExtensionNames = ppEnabledExtensionNames,
+    });
+    return VkZeroInstance{
+        .instance = instance,
+        .physicalDevices = instance.enumeratePhysicalDevices(),
+        .physicalDeviceGroups = instance.enumeratePhysicalDeviceGroups(),
+    };
+}
+
+inline VkZeroDevice
+createDevice(const VkZeroInstance &instance,
+             const vk::PhysicalDeviceFeatures *pEnabledFeatures,
+             std::vector<vk::DescriptorPoolSize> poolSizes) {
+    const auto physicalDeviceIndex = 0;
+    if (instance.physicalDevices.size() < physicalDeviceIndex + 1) {
         throw static_cast<int32_t>(-1);
     }
-    const auto &familyProperties = device.queueFamilyProperties[0];
+    const auto queueFamilyIndex = 0;
+    const auto queueIndex = 0;
+    const auto queuePriority = 0.f;
+    vk::DeviceQueueCreateInfo queueCreateInfo{
+        .queueFamilyIndex = queueFamilyIndex,
+        .queueCount = 1,
+        .pQueuePriorities = &queuePriority};
+    auto device = instance.physicalDevices[physicalDeviceIndex].createDevice(
+        vk::DeviceCreateInfo{
+            .queueCreateInfoCount = 1,
+            .pQueueCreateInfos = &queueCreateInfo,
+            .pEnabledFeatures = pEnabledFeatures,
+        });
+    if (instance.physicalDevices[physicalDeviceIndex]
+            .getQueueFamilyProperties()
+            .size() < queueFamilyIndex + 1) {
+        throw -1;
+    }
+    const auto queueFamilyProperties =
+        instance.physicalDevices[physicalDeviceIndex]
+            .getQueueFamilyProperties()[queueFamilyIndex];
     const auto queueFlags = vk::QueueFlagBits::eGraphics |
                             vk::QueueFlagBits::eCompute |
                             vk::QueueFlagBits::eTransfer;
-    if ((familyProperties.queueFlags & queueFlags) != queueFlags) {
-        throw static_cast<int32_t>(-1);
+    if ((queueFamilyProperties.queueFlags & queueFlags) != queueFlags &&
+        queueFamilyProperties.queueCount < queueIndex + 1) {
+        throw -1;
     }
-    return VkZeroQueue{
-        .queue = device.device.getQueue(0, 0), .familyIndex = 0, .index = 0};
+    VmaAllocatorCreateInfo allocatorCreateInfo = {
+        .physicalDevice = instance.physicalDevices[physicalDeviceIndex],
+        .device = device,
+        .instance = instance.instance,
+        .vulkanApiVersion = VK_API_VERSION_1_1,
+    };
+    VmaAllocator allocator;
+    if (vmaCreateAllocator(&allocatorCreateInfo, &allocator) != VK_SUCCESS) {
+        throw -1;
+    }
+    return VkZeroDevice{
+        .index = physicalDeviceIndex,
+        .device = device,
+        .allocator = allocator,
+        .descriptorPool =
+            device.createDescriptorPool(vk::DescriptorPoolCreateInfo{
+                .flags = vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet,
+                .maxSets = 8192,
+                .poolSizeCount = static_cast<uint32_t>(poolSizes.size()),
+                .pPoolSizes = poolSizes.data(),
+            }),
+        .queue =
+            VkZeroQueue{
+                .familyIndex = queueFamilyIndex,
+                .index = queueIndex,
+                .priority = queuePriority,
+                .queue = device.getQueue(queueFamilyIndex, queueIndex),
+                .commandPool =
+                    device.createCommandPool(vk::CommandPoolCreateInfo{
+                        .flags =
+                            vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
+                        .queueFamilyIndex = queueFamilyIndex,
+                    }),
+            },
+    };
 }
 
 #else
@@ -104,5 +188,18 @@ VkZeroQueue getGraphicsComputeTransferQueue(const VkZeroDevice &device) {
 #define vec4 (float4)
 
 #endif
+
+typedef struct {
+    uint64_t state;
+    uint64_t inc;
+} pcg32_random_t;
+
+uint32_t pcg32_random_r(pcg32_random_t *rng) {
+    uint64_t oldstate = rng->state;
+    rng->state = oldstate * 6364136223846793005ULL + (rng->inc | 1);
+    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+    uint32_t rot = oldstate >> 59u;
+    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
 
 #endif
