481993cc3677da8ecacd02d394f8ccdd833dd60f
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-05-24 06:41:02 +0300

    Use consistent naming

--- src/lib.h
+++ src/lib.h
@@ -200,17 +200,17 @@ createDevice(const VkZeroInstance &instance,
 
 #endif
 
-typedef struct {
+struct Pcg32Random {
     uint64_t state;
     uint64_t inc;
-} pcg32_random_t;
+};
 
-inline uint32_t pcg32_random_r(pcg32_random_t *&rng) {
-    auto oldstate = rng->state;
-    rng->state = oldstate * 6364136223846793005ULL + (rng->inc | 1);
-    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
-    uint32_t rot = oldstate >> 59u;
-    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+inline uint32_t Pcg32(Pcg32Random *&rng) {
+    auto oldState = rng->state;
+    rng->state = oldState * 6364136223846793005ULL + (rng->inc | 1);
+    uint32_t xorShifted = ((oldState >> 18u) ^ oldState) >> 27u;
+    uint32_t rot = oldState >> 59u;
+    return (xorShifted >> rot) | (xorShifted << ((-rot) & 31));
 }
 
 #endif
