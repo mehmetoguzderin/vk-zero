cf071e1c8f2d0839773a87db440498c0448367eb
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-06-04 00:52:45 +0300

    Enable all build

--- src/bin/graphics.cpp
+++ src/bin/graphics.cpp
@@ -3,9 +3,10 @@
 
 int main(int argc, char *argv[]) {
     try {
+        std::vector<const char *> enabledLayers{};
         std::vector<const char *> enabledExtensions{};
         auto window = createWindow(enabledExtensions, 512, 512);
-        const auto instance = createInstance(enabledExtensions);
+        const auto instance = createInstance(enabledLayers, enabledExtensions);
         const auto surface = createSurface(window, instance);
         const auto device = createDevice(
             instance,
